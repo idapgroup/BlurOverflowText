fdbe3f7108c391b4b795c179331df88d419698d1
Jeketos <jeketos@gmail.com>
2024-07-10 15:12:26 +0300

    Update compose

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -53,11 +53,11 @@ android {
 
 dependencies {
 //    implementation(project(":bluroverflowtext"))
-    implementation("com.github.idapgroup:BlurOverflowText:0.1.3")
+    implementation("com.github.idapgroup:BlurOverflowText:0.1.4")
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
     implementation("androidx.activity:activity-compose:1.9.0")
-    implementation(platform("androidx.compose:compose-bom:2024.05.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
