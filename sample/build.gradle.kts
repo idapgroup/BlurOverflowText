3eb0a6cdff0e475ade5fe8d5146ed1b86dbe1775
Jeketos <jeketos@gmail.com>
2024-08-15 15:28:14 +0300

    0.1.5 - Update kotlin

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -52,11 +52,11 @@ android {
 }
 
 dependencies {
-//    implementation(project(":bluroverflowtext"))
-    implementation("com.github.idapgroup:BlurOverflowText:0.1.4")
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
-    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation(project(":bluroverflowtext"))
+    implementation("com.github.idapgroup:BlurOverflowText:0.1.5")
+//    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
+    implementation("androidx.activity:activity-compose:1.9.1")
     implementation(platform("androidx.compose:compose-bom:2024.06.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
